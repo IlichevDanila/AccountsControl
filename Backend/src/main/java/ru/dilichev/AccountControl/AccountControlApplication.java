da68fd7478c36cace0556a126a99bf6448e85916
Ilichev Danila <you-may@mail.ru>
2022-03-22 14:46:42 +0300

    Backend side added (IntelliJ)

---
+++ Backend/src/main/java/ru/dilichev/AccountControl/AccountControlApplication.java
@@ -0,0 +1,13 @@
+package ru.dilichev.AccountControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountControlApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/ru/dilichev/AccountControl/AccountControlApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dilichev.AccountControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
